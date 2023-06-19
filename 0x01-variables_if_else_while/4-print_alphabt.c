99ed79509be692d050b83dd98dc145bf7d6b5948
MsLakes <karabolakes@outlook.com>
2023-06-19 12:46:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char low, e, q;
+	
+	e = 'e';
+	q = 'q';
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low !=q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
