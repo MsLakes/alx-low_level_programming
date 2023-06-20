9ef67ef3adb48ef9554daab88dc6a8d270dbd2bd
MsLakes <karabolakes@outlook.com>
2023-06-20 08:39:16 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int d, i;
+	
+	for (d = '0'; d < '9'; d++)
+	{
+	for (i = d + 1; i <= 10; i++)
+	{
+	if (i != d)
+	{
+	putchar(d);
+	putchar(i);
+	if (d == '8' && i == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
