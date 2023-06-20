2fb3bbae5a2230633470d7550b547e3bfcbca106
MsLakes <karabolakes@outlook.com>
2023-06-20 10:09:29 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k, n, l;
+
+	for (k = '0'; k < '9'; k++)
+	{
+	for (n = k + 1; n < '9'; n++)
+	{
+	for (l = n +1; l < '9'; l++)
+	{
+	if (k < n && n < l)
+	{
+	putchar(k);
+	putchar(n);
+	putchar(l);
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
