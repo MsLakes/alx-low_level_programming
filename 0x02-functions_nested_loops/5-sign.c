224da612730958136a5fc410eabcc4ec418f0680
MsLakes <karabolakes@outlook.com>
2023-06-21 03:54:31 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print _sign - Prints the sign of a number
+ * print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or 0 for anything else
  */
