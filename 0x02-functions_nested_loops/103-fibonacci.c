bf8e26292dc3232b262c9524b79e11ec028ff202
MsLakes <karabolakes@outlook.com>
2023-06-21 13:28:05 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Create the sum of Even fibonacci numbers
+ * less than 4000000
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, kam = k;
+
+	while (k + j < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	kam += k;
+	j = k -j;
+	++i;
+	}
+	printf("%ld\n", kam);
+	return (0);
+}
