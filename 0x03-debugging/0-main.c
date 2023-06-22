ff3d5227d4ea019a8a76cb911630bdab1370da0d
MsLakes <karabolakes@outlook.com>
2023-06-22 13:36:11 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Test function that prints if integer is positive or negative
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_negative(i):
+	return (0);
+}
