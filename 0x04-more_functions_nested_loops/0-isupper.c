98e77aa2965d448236d6d904f0620415b3a6eb72
MsLakes <karabolakes@outlook.com>
2023-06-23 08:41:41 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Check for uppercase character
+ * @k - The number to be checked
+ * Return: 1 for uppercase or 0 for anything else
+ */
+int _isupper(int k)
+{
+	if (k >= 65 && k <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
