788bece45713d3370abbf0579195dc8cc951ab7a
MsLakes <karabolakes@outlook.com>
2023-07-04 14:56:12 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memcpy - Entry point
+ * @dest: input memory area
+ * @src: input
+ * @n: bytes
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+		dest[k] = src[k];
+	return(dest);
+}	
