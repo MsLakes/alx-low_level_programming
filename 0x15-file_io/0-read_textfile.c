b62ec408b242e0c2dd4bf8051aef148d904497d5
MsLakes <karabolakes@outlook.com>
2023-10-06 11:18:24 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads text from a file and prints it to the standard output.
- * @filename: Name of the file to read.
- * @letters: Maximum number of letters to read and print.
+ * read_textfile - Display content from a file on STDOUT.
+ * @filename: The name of the text file to read.
+ * @letters: The maximum number of characters to read.
  * 
- * Return: Number of bytes read and printed, or 0 on failure or if filename is NULL.
+ * Return: w - The actual number of characters displayed.
+ *         0 if an error occurs or if filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd, read_bytes, written_bytes;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    if (!filename)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
 
-    buf = malloc(sizeof(char) * letters);
-    if (!buf) {
-        close(fd);
-        return (0);
-    }
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    read_bytes = read(fd, buf, letters);
-    written_bytes = write(STDOUT_FILENO, buf, read_bytes);
+	free(buf);
+	close(fd);
 
-    free(buf);
-    close(fd);
-    
-    return (written_bytes);
+	return (w);
 }
